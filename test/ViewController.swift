c00ef75c74f995cc35a4a780a41e0e6cb5c82ac9
Pawel Bala <pawelbala@motorolasolutions.com>
2018-11-28 20:46:18 -0600

    initial commit

---
+++ test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Pawel Bala on 11/28/18.
+//  Copyright © 2018 Pawel Bala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
